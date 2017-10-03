d1655f649c5bebbccc03fb85271e56af9670283e
Aki Vehtari <Aki.Vehtari@aalto.fi>
2017-10-03 16:16:35 +0300

    fixed

--- demos_ch4/demo4_1.R
+++ demos_ch4/demo4_1.R
@@ -5,6 +5,7 @@
 library(ggplot2)
 library(gridExtra)
 library(tidyr)
+library(MASS)
 
 # Normal approximaton for Bioassay model.
 
@@ -15,7 +16,7 @@ df1 <- data.frame(
   y = c(0, 1, 3, 5)
 )
 
-# compute the posterior density in grid
+# compute the posterior density in a grid
 #  - usually should be computed in logarithms!
 #  - with alternative prior, check that range and spacing of A and B
 #    are sensible
@@ -74,7 +75,7 @@ sam <- ggplot(data = data.frame(samp_A, samp_B)) +
 his <- ggplot() +
   geom_histogram(aes(samp_ld50), binwidth = 0.04,
                  fill = 'steelblue', color = 'black') +
-  coord_cartesian(xlim = c(-0.5, 0.5)) +
+  coord_cartesian(xlim = c(-0.8, 0.8)) +
   labs(x = 'LD50 = -alpha/beta')
 
 # define the function to be optimized
@@ -94,28 +95,20 @@ S <- solve(optim_res$hessian)
 dmvnorm <- function(x, mu, sig)
   exp(-0.5*(length(x)*log(2*pi) + log(det(sig)) + (x-mu)%*%solve(sig, x-mu)))
 
-# evaluate likelihood at points (cA,cB)
+# evaluate likelihood at points (cA,cB) 
+# this is just for illustration and would not be needed otherwise
 p <- apply(cbind(cA, cB), 1, dmvnorm, w, S)
 
-# sample from the grid (with replacement)
-nsamp <- 1000
-samp_indices <- sample(length(p), size = nsamp,
-                       replace = T, prob = p/sum(p))
-
-samp_A <- cA[samp_indices[1:nsamp]]
-samp_B <- cB[samp_indices[1:nsamp]]
-# add random jitter, see BDA3 p. 76
-samp_A <- samp_A + runif(nsamp, A[1] - A[2], A[2] - A[1])
-samp_B <- samp_B + runif(nsamp, B[1] - B[2], B[2] - B[1])
+# sample from the multivariate normal 
+normsamp <- mvrnorm(nsamp, w, S)
 
 # samples of LD50 conditional beta > 0
 # Normal approximation does not take into account that the posterior
 # is not symmetric and that there is very low density for negative
 # beta values. Based on the draws from the normal approximation
-# is is estimated that there is about 6% probability that beta is negative!
-bpi <- samp_B > 0
-samp_ld50 <- -samp_A[bpi]/samp_B[bpi]
-
+# is is estimated that there is about 5% probability that beta is negative!
+bpi <- normsamp[,2] > 0
+normsamp_ld50 <- -normsamp[bpi,1]/normsamp[bpi,2]
 
 # create a plot of the posterior density
 pos_norm <- ggplot(data = data.frame(cA ,cB, p), aes(x = cA, y = cB)) +
@@ -127,18 +120,17 @@ pos_norm <- ggplot(data = data.frame(cA ,cB, p), aes(x = cA, y = cB)) +
   scale_alpha(range = c(0, 1), guide = F)
 
 # plot of the samples
-sam_norm <- ggplot(data = data.frame(samp_A, samp_B)) +
+sam_norm <- ggplot(data = data.frame(samp_A=normsamp[,1], samp_B=normsamp[,2])) +
   geom_point(aes(samp_A, samp_B), color = 'blue', size = 0.3) +
   coord_cartesian(xlim = xl, ylim = yl) +
   labs(x = 'alpha', y = 'beta')
 
 # plot of the histogram of LD50
 his_norm <- ggplot() +
-  geom_histogram(aes(samp_ld50), binwidth = 0.04,
+  geom_histogram(aes(normsamp_ld50), binwidth = 0.04,
                  fill = 'steelblue', color = 'black') +
-  coord_cartesian(xlim = c(-0.5, 0.5)) +
+  coord_cartesian(xlim = c(-0.8, 0.8)) +
   labs(x = 'LD50 = -alpha/beta, beta > 0')
 
 # combine the plots
 grid.arrange(pos, sam, his, pos_norm, sam_norm, his_norm, ncol = 3)
-
