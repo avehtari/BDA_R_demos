845cba2665f2b9e8bc7d51dc8018c2c430aeced0
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-02-09 00:25:34 +0100

    add some markdown and spin to html

---
+++ demos_ch5/README.md
@@ -0,0 +1,6 @@
+# Links to html versions of demos
+
+- [demo5_1](https://rawgit.com/avehtari/BDA_R_demos/master/demos_ch5/demo5_1.html)
+- [demo5_2](https://rawgit.com/avehtari/BDA_R_demos/master/demos_ch5/demo5_2.html)
+
+(Using rawgit as github doesn't display html or Rmd correctly)
--- demos_ch5/demo5_2.R
+++ demos_ch5/demo5_2.R
@@ -1,22 +1,27 @@
-# Bayesian data analysis
-# Aki Vehtari <Aki.Vehtari@aalto.fi>
-# Markus Paasiniemi <Markus.Paasiniemi@aalto.fi>
+#' ---
+#' title: "Bayesian data analysis demo 4.1"
+#' author: "Aki Vehtari, Markus Paasiniemi"
+#' date: "`r format(Sys.Date())`"
+#' ---
 
+#' ## Hierarchical model for SAT-example data (BDA3, p. 102)
+
+#' ggplot2, grid, and gridExtra are used for plotting, tidyr for
+#' manipulating data frames
+#+ setup, message=FALSE, error=FALSE, warning=FALSE
 library(ggplot2)
 library(gridExtra)
 library(tidyr)
+library(here)
 
-# Hierarchical model for SAT-example data (BDA3, p. 102)
-
+#' Data 
 y <- c(28,8,-3,7,-1,1,18,12)
 s <- c(15,10,16,11,9,11,10,18)
 
-# Plot data, use normpdf to plot both the y_j and sigma_j
+#' Plot data, use normpdf to plot both the y_j and sigma_j
 x <- seq(-40, 60, length.out = 500)
-
 df_sep <- mapply(function(y, s, x) dnorm(x, y, s), y, s, MoreArgs = list(x = x)) %>%
   as.data.frame() %>% setNames(LETTERS[1:8]) %>% cbind(x) %>% gather(school, p, -x)
-
 labs1 <- c('Other Schools', 'School A')
 plot_sep <- ggplot(data = df_sep) +
   geom_line(aes(x = x, y = p, color = (school=='A'), group = school)) +
@@ -24,10 +29,12 @@ plot_sep <- ggplot(data = df_sep) +
   scale_y_continuous(breaks = NULL) +
   scale_color_manual(values = c('blue','red'), labels = labs1) +
   theme(legend.background = element_blank(), legend.position = c(0.8,0.9))
+#+ fig.height=4
 plot_sep
 
+#' A pooled model
 df_pool <- data.frame(x = x, p = dnorm(x, sum(y/s^2)/sum(1/s^2), sqrt(1/sum(1/s^2))))
-
+#' Create plot for the pooled model
 plot_pool <- ggplot(data = df_pool) +
   geom_line(aes(x = x, y = p, color = '1')) +
   labs(x = 'Treatment effect', y = '', title = 'Pooled model', color = '') +
@@ -35,13 +42,13 @@ plot_pool <- ggplot(data = df_pool) +
   scale_color_manual(values = 'red', labels = 'All schools') +
   theme(legend.background = element_blank(), legend.position = c(0.7,0.9))
 
-# Load the pre-computed results for the hierarchical model
-# Replace this with your own code in the related exercise
-load('demo5_2.RData')
+#' Load the pre-computed results for the hierarchical model
+#' Replace this with your own code in the related exercise
+load(here("demos_ch5","demo5_2.RData"))
 
+#' Create plot for the hierarchical model
 df_hier <- as.data.frame(t(pxm)) %>% setNames(LETTERS[1:8]) %>%
   cbind(x) %>% gather(school, p, -x)
-
 plot_hier <- ggplot(data = df_hier) +
   geom_line(aes(x = x, y = p, color = (school=='A'), group = school)) +
   labs(x = 'Treatment effect', y = '', title = 'Hierarchical model', color = '') +
@@ -49,13 +56,12 @@ plot_hier <- ggplot(data = df_hier) +
   scale_color_manual(values = c('blue','red'), labels = labs1) +
   theme(legend.background = element_blank(), legend.position = c(0.8,0.9))
 
-# Plot separate, pooled, and hierarchical model
+#' Plot separate, pooled, and hierarchical model
+#+ fig.height=6
 grid.arrange(plot_sep, plot_pool, plot_hier)
 
-# Various marginal and conditional posterior summaries
-
+#' Various marginal and conditional posterior summaries
 df_margpost = data.frame(x = t(tt), p = t(tp))
-
 title1 <- 'Marginal posterior density p(tau|y)'
 plot_margpost <-
   ggplot(data = df_margpost) +
@@ -66,7 +72,7 @@ plot_margpost <-
 df_condmeans <- as.data.frame(t(tm)) %>% setNames(LETTERS[1:8]) %>%
   cbind(x = t(tt)) %>% gather(school, p, -x)
 
-yl <- c(-50, 50)
+yl <- c(-5, 40)
 title2 <- 'Conditional posterior means of effects E[theta_j|tau,y]'
 plot_condmeans <- ggplot(data = df_condmeans) +
   geom_line(aes(x = x, y = p, color = (school=='A'), group = school)) +
@@ -78,6 +84,7 @@ plot_condmeans <- ggplot(data = df_condmeans) +
 df_condsds <- as.data.frame(t(tsd)) %>% setNames(LETTERS[1:8]) %>%
   cbind(x = t(tt)) %>% gather(school, p, -x)
 
+yl <- c(0, 25)
 title3 <- 'Conditional posterior standard deviations of effects sd[theta_j|tau,y]'
 plot_condsds <- ggplot(data = df_condsds) +
   geom_line(aes(x = x, y = p, color = (school=='A'), group = school)) +
@@ -86,6 +93,6 @@ plot_condsds <- ggplot(data = df_condsds) +
   scale_color_manual(values = c('blue','red'), labels = labs1) +
   theme(legend.background = element_blank(), legend.position = c(0.8,0.9))
 
-# Plot the posterior summaries
+#' Plot the posterior summaries
+#+ fig.height=8
 grid.arrange(plot_margpost, plot_condmeans, plot_condsds)
-
