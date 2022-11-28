00ca647c5889030029bf3126e2cf7058338e6471
Aki Vehtari <Aki.Vehtari@aalto.fi>
2022-11-28 12:53:42 +0200

    updated trafficdeaths demo

--- demos_rstan/trafficdeaths.R
+++ demos_rstan/trafficdeaths.R
@@ -1,27 +1,27 @@
----
-title: "Bayesian data analysis - traffic deaths in Finland"
-author: "Aki Vehtari"
-date: "First version 2017-09-28. Last modified `r format(Sys.Date())`."
-output:
-  html_document:
-    fig_caption: yes
-    toc: TRUE
-    toc_depth: 2
-    number_sections: TRUE
-    toc_float:
-      smooth_scroll: FALSE
-    theme: readable
-    code_download: true
----
-# Setup  {.unnumbered}
+#' ---
+#' title: "Bayesian data analysis - traffic deaths in Finland"
+#' author: "Aki Vehtari"
+#' date: "First version 2017-09-28. Last modified `r format(Sys.Date())`."
+#' output:
+#'   html_document:
+#'     fig_caption: yes
+#'     toc: TRUE
+#'     toc_depth: 2
+#'     number_sections: TRUE
+#'     toc_float:
+#'       smooth_scroll: FALSE
+#'     theme: readable
+#'     code_download: true
+#' ---
 
-```{r setup, include=FALSE}
+#' # Setup  {.unnumbered}
+
+#+ setup, include=FALSE
 knitr::opts_chunk$set(cache=FALSE, message=FALSE, error=FALSE, warning=TRUE, out.width='95%')
-```
 
-**Load packages**
 
-```{r comment=NA}
+#' **Load packages**
+#+  comment=NA
 library(ggplot2)
 library(tidyr)
 library(gridExtra)
@@ -33,32 +33,30 @@ library(patchwork)
 library(loo)
 library(rprojroot)
 root<-has_file(".BDA_R_demos_root")$make_fix_file()
-```
 
-# Introduction
 
-This notebook demonstrates time series analysis for traffic deaths per
-year in Finland. Currently when the the number of traffic deaths
-during previous year are reported, the press release claims that the
-the traffic safety in Finland has improved or worsened depending
-whether the number is smaller or larger than the year before. Time
-series analysis can be used to separate random fluctuation from the
-slowly changing traffic safety.
-
-# Data
-
-Read the data (there would data for earlier years, too, but this is
-sufficient for the demonstration)
-
-```{r }
+#' # Introduction
+#' 
+#' This notebook demonstrates time series analysis for traffic deaths per
+#' year in Finland. Currently when the the number of traffic deaths
+#' during previous year are reported, the press release claims that the
+#' the traffic safety in Finland has improved or worsened depending
+#' whether the number is smaller or larger than the year before. Time
+#' series analysis can be used to separate random fluctuation from the
+#' slowly changing traffic safety.
+#' 
+#' # Data
+#' 
+#' Read the data (there would data for earlier years, too, but this is
+#' sufficient for the demonstration)
+#+ 
 # file preview shows a header row
 deaths <- read.csv(root("demos_rstan", "trafficdeaths.csv"), header = TRUE)
 head(deaths)
-```
 
-First plot just the data.
 
-```{r }
+#' First plot just the data.
+#+
 deaths |>
   ggplot(aes(x=year, y=deaths)) +
   geom_point() +
@@ -70,25 +68,24 @@ deaths |>
   geom_line() +
   labs(y = 'Traffic deaths', x= "Year") +
   guides(linetype = "none")
-```
 
-# Poisson regression model
 
-The number of deaths is count data, so we use Poisson observation
-model.  We first fit log-linear model for the Poisson intensity, which
-corresponds to assuming constant proportional change in the rate.
+#' # Poisson regression model
+#' 
+#' The number of deaths is count data, so we use Poisson observation
+#' model.  We first fit log-linear model for the Poisson intensity, which
+#' corresponds to assuming constant proportional change in the rate.
 
-```{r }
+#+ 
 fit_lin <- stan_glm(deaths ~ year, data=deaths, family=poisson,
  	            refresh=1000, iter=1000, chains=4, seed=583829, refresh=0)
-```
 
-ESS's and Rhat's are ok (see, e.g., [RStan
-workflow](http://mc-stan.org/users/documentation/case-studies/rstan_workflow.html)). Let's
-look at the posterior predictive distribution (median and 5% and 95%
-intervals).
 
-```{r }
+#' ESS's and Rhat's are ok (see, e.g., [RStan
+#' workflow](http://mc-stan.org/users/documentation/case-studies/rstan_workflow.html)). Let's
+#' look at the posterior predictive distribution (median and 5% and 95%
+#' intervals).
+#+ 
 x_predict <- seq(1993,2030)
 N_predict <- length(x_predict)
 y_predict_lin <- posterior_predict(fit_lin, newdata=data.frame(year=x_predict))
@@ -101,21 +98,18 @@ pfit <- ggplot() +
   labs(x = 'Year', y = 'Traffic deaths') +
   guides(linetype = F)
 (pfit)
-```
 
-Next we fit a non-linear spline model with `stan_gamm4`
 
-```{r }
+#' Next we fit a non-linear spline model with `stan_gamm4`
+#+ 
 fit_gam <- stan_gamm4(deaths ~ year + s(year), data=deaths,
                       family=poisson, adapt_delta=0.999, 
                       refresh=1000, iter=2000, chain=4, seed=583829, refresh=0)
-```
 
-ESS is clearly smaller than for the linear model, but Rhat's are ok.
-
-Let's look at the posterior predictive distribution.
-
-```{r }
+#' ESS is clearly smaller than for the linear model, but Rhat's are ok.
+#' 
+#' Let's look at the posterior predictive distribution.
+#+ 
 x_predict=seq(1993,2030)
 N_predict=length(x_predict)
 y_predict_gam <- posterior_predict(fit_gam, newdata=data.frame(year=x_predict))
@@ -128,16 +122,15 @@ pfit <- ggplot() +
   labs(x = 'Year', y = 'Traffic deaths') +
   guides(linetype = F)
 (pfit)
-```
 
-The predictive median is clearly nonlinear. The predictive mean for
-future years stays at the same level as the most recent observations,
-but uncertainty increases quickly.
 
-Finally we fit Gaussian process centered on linear model. We use
-brms for this:
-
-```{r comment=NA}
+#' The predictive median is clearly nonlinear. The predictive mean for
+#' future years stays at the same level as the most recent observations,
+#' but uncertainty increases quickly.
+#' 
+#' Finally we fit Gaussian process centered on linear model. We use
+#' brms for this:
+#+  comment=NA
 fit_gp <- brm(deaths ~ year + gp(year), data=deaths,
                       family=poisson, adapt_delta=0.95, 
               refresh=1000, iter=2000, chain=4, seed=583829, refresh=0,
@@ -155,26 +148,23 @@ pfit <- ggplot() +
   labs(x = 'Year', y = 'Traffic deaths') +
   guides(linetype = F)
 (pfit)
-```
 
-Finally we compare models using PSIS-LOO predictive performance estimates.
+#' Finally we compare models using PSIS-LOO predictive performance estimates.
 
-```{r }
+#+ 
 (loo_lin<-loo(fit_lin, save_psis=TRUE))
 (loo_gam<-loo(fit_gam, save_psis=TRUE))
 (loo_gp<-loo(fit_gp, save_psis=TRUE))
 
 loo_compare(loo_lin, loo_gam, loo_gp)
-```
 
-There are no practical differences in predictive performance, which is
-partially due to small number of observations. Based on the posterior
-predictive distributions there are clear differences in the future
-predictions.
-We can also look at the calibration of leave-one-out predictive
-distributions
+#' There are no practical differences in predictive performance, which is
+#' partially due to small number of observations. Based on the posterior
+#' predictive distributions there are clear differences in the future
+#' predictions.
 
-```{r }
+#' We can also look at the calibration of leave-one-out predictive
+#' distributions
 pi_lin <- ppc_loo_intervals(deaths$deaths,
                     y_predict_lin[,1:29],
                     psis_object=loo_lin$psis_object)+
@@ -197,20 +187,20 @@ pq_gp <- ppc_loo_pit_qq(deaths$deaths,
 
 
 (pi_lin+pq_lin+pi_gp+pq_gp)
-```
 
-There is a small difference in favor of GP model.
-<br />
+#' There is a small difference in favor of GP model.
 
-# Licenses {.unnumbered}
+#' <br />
+#' 
+#' # Licenses {.unnumbered}
+#' 
+#' * Code &copy; 2017-2020, Aki Vehtari, licensed under BSD-3.
+#' * Text &copy; 2017-2020, Aki Vehtari, licensed under CC-BY-NC 4.0.
+#' 
+#' # Original Computing Environment {.unnumbered}
 
-* Code &copy; 2017-2020, Aki Vehtari, licensed under BSD-3.
-* Text &copy; 2017-2020, Aki Vehtari, licensed under CC-BY-NC 4.0.
-
-# Original Computing Environment {.unnumbered}
-
-```{r }
+#+ 
 sessionInfo()
-```
 
-<br />
+
+#' <br />
