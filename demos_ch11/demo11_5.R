a00d1f99de4e59c778343e1271b1f06498fb5fe5
Aki Vehtari <Aki.Vehtari@aalto.fi>
2023-10-01 18:57:19 +0300

    more summarise options and bayesplot

--- demos_ch11/demo11_5.R
+++ demos_ch11/demo11_5.R
@@ -40,6 +40,11 @@ summarise_draws(thetas)
 #' lecture we can use additionl arguments
 summarise_draws(thetas, Rhat=rhat_basic, ESS=ess_basic)
 
+#' Compute mean and 5%- and 95%-quantiles and corresponding MCSE's
+summarise_draws(thetas, mean, mcse_mean,
+                ~quantile(.x, probs = c(0.05, 0.95)),
+                ~mcse_quantile(.x, probs = c(0.05, 0.95)))
+
 #' summarise_draws() function coerced the R matrix to a posterior
 #' object behind the scenes, but we can also convert it explicitlu to
 #' draws object that is better aware of number of iterations and
@@ -48,6 +53,8 @@ summarise_draws(thetas, Rhat=rhat_basic, ESS=ess_basic)
 #' posterior objects easier than plain R matrix and array types.
 thetas <- as_draws_df(thetas)
 
+
+
 #' Illustration with several chains
 N <- 1000 # number of iterations per chain
 M <- 4 # number of chains
@@ -55,8 +62,8 @@ D <- 2 # number of parameters
 # 3-D array with dimensions ‘"iteration"’, ‘"chain"’, and ‘"variable"’
 thetas <- array(dim=c(N,M,D))
 # Fill the array
-for (n in 1:N) {
-  for (m in 1:M) {
+for (m in 1:M) { # loop over chains
+  for (n in 1:N) { # loop over iterations
     thetas[n,m,] <- rnorm(n=2, mean=0, sd=1)
   }
 }
@@ -69,6 +76,11 @@ summarise_draws(thetas)
 #' lecture we can use additionl arguments
 summarise_draws(thetas, Rhat=rhat_basic, ESS=ess_basic)
 
+#' Compute mean and 5%- and 95%-quantiles and corresponding MCSE's
+summarise_draws(thetas, mean, mcse_mean,
+                ~quantile(.x, probs = c(0.05, 0.95)),
+                ~mcse_quantile(.x, probs = c(0.05, 0.95)))
+
 #' summarise_draws() function coerced the R 3-D array to a posterior
 #' object behind the scenes, but we can also convert it explicitlu to
 #' draws object that is better aware of number of iterations and
@@ -76,3 +88,34 @@ summarise_draws(thetas, Rhat=rhat_basic, ESS=ess_basic)
 #' posterior package provides several functions that make manipulating
 #' posterior objects easier than plain R matrix and array types.
 thetas <- as_draws_df(thetas)
+variables(thetas) <- c("theta1","theta2")
+
+#' bayesplot package provides Some useful plots to analyse your MCMC
+#' draws.  You don't need to include all of these in your assignment
+#' or project reports, but they are shown as seeing the plo for your
+#' own Metropolis algorithm helps to gain better understanding.
+#'
+#' Trace plots
+#' 
+mcmc_trace(thetas)
+#'
+#' Autocorrelation plots
+#' 
+mcmc_acf(thetas)
+#'
+#' Density plots with separate density for each chain
+#' 
+mcmc_dens_overlay(thetas)
+#'
+#' Density plots using all chains
+#' 
+mcmc_dens(thetas)
+#'
+#' Density plots with median and 50% central interval (see ?mcmc_areas
+#' for more options)
+#' 
+mcmc_areas(thetas)
+#' 
+#' Scatter plots and histograms (works also for more than two variables)
+#' 
+mcmc_pairs(thetas)
